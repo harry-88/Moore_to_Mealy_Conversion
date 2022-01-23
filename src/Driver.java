eb57fa0064d3b46386b653fa6d65a0b1b52e307d
Muhammad Haris <67223517+harry-88@users.noreply.github.com>
2022-01-23 11:06:02 +0500

    added some new comments

--- src/Driver.java
+++ src/Driver.java
@@ -6,15 +6,19 @@ public class Driver {
     static int numberOfStates;
     static String[] latter;
     static String startingState;
-    static ArrayList<Moore> mooreArrayList;
-    static ArrayList<Mealy> mealyArrayList;
+    static ArrayList<Moore> mooreArrayList;//store the moore data for every state
+    static ArrayList<Mealy> mealyArrayList;//store the mealy data for every data
 
-    public static void displayMooreData()
+    public static void displayMooreData()//display moore data
     {
+        //make header of the moore table
         System.out.print("Present State\t\t\tNew State\t\t\tOutput\n\t\t\t\t\t\t");
         for (int i = 0;i< latter.length;i++)
             System.out.print(latter[i]+"\t");
         System.out.println();
+        //end of header of moore table
+
+        //display record of the moore data
         for (int i = 0;i<mooreArrayList.size();i++)
         {
             System.out.print(mooreArrayList.get(i).getStartingState()+"\t\t\t\t\t\t");
@@ -23,25 +27,32 @@ public class Driver {
             System.out.println("\t\t\t"+mooreArrayList.get(i).output);
 
         }
+        //end of moore data
     }
 
-    public static void displayMealyData()
+    public static void displayMealyData()//display the mealy table
     {
+        //make mealy table header
         System.out.print("Present State\t\t\tNew State\n\t\t\t\t\t\t");
         for (int i = 0;i< latter.length;i++)
             System.out.print(latter[i]+"\tOP\t");
         System.out.println();
+        //mealy table header end
+
+        //display mealy record
         for (int i = 0;i<mealyArrayList.size();i++)
         {
             System.out.print(mealyArrayList.get(i).getStartingState()+"\t\t\t\t\t\t");
             for (int j = 0;j < latter.length;j++)
                 System.out.print(mealyArrayList.get(i).getEndingState()[j]+"\t"+mealyArrayList.get(i).output[j]+"\t");
             System.out.println();
-
         }
+        //end of mealy data
     }
     public static boolean checkValidState(String state)//check the user entered a valid state
     {
+
+        //check the validation that user entered the valid state
 
         String line1 = "";
         for (int i = 0;i<numberOfStates;i++) {
@@ -52,24 +63,28 @@ public class Driver {
         }
             return false;
     }
-    public static void getMooreData()
+    public static void getMooreData()//take the moore data
     {
 
+        //display all valid states that user can add
         System.out.println("the states are : ");
         for (int i = 0;i<numberOfStates;i++)
             System.out.print("q"+i+"\t");
         System.out.println();
+        //end the display state
         boolean checkValidity = false;
         String line;
         String end;
+
+        //take all record for all states
         for (int i = 0;i<numberOfStates;i++)
         {
 
                 line = "q"+i;
                 startingState = line;
 
-
-            String[] endingState = new String[latter.length];
+                    //take the state in which it move after reading the latter
+                    String[] endingState = new String[latter.length];
                     for (int j = 0;j< latter.length;j++)
                     {
                         System.out.println("enter ending state of "+ line+ " after reading "+ latter[j]);
@@ -77,18 +92,19 @@ public class Driver {
                         if (checkValidState(end))
                             endingState[j] = end;
                         else   System.out.println("you entered invalid state");
-                    }
+                    }//end of taking the ending states
                     System.out.println("enter the output of the state "+line);
                     String output = input.next();
-                    mooreArrayList.add(new Moore(startingState,endingState,output));
+                    mooreArrayList.add(new Moore(startingState,endingState,output));//add moore data to arraylist
 
 
         }
     }
 
-    public static void getMealyData()
+    public static void getMealyData()//take the mealy data
     {
 
+        //display all valid states that user can add
         System.out.println("the states are : ");
         for (int i = 0;i<numberOfStates;i++)
             System.out.print("q"+i+"\t");
